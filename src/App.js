d01ecea2e5c94631caf185a784e4149bc140ed2b
Marko Tanasijevic <tanasije286@gmail.ca>
2024-10-17 13:15:31 -0400

    Functionality added for switching between X's and O's

--- src/App.js
+++ src/App.js
@@ -10,12 +10,14 @@ function Square({ value , onSquareClick}) {
 }
 
 function Board() {
+    const [xIsNext, setXIsNext] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null));
 
     function handleClick(i){
         const nextSquares = squares.slice();
-        nextSquares[i] = "X";
+        (xIsNext ? nextSquares[i] = 'X' : nextSquares[i] = 'O');
         setSquares(nextSquares);
+        setXIsNext(!xIsNext);
     }
 
   return (
