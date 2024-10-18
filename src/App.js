ce5079c2f0197b6c77ba632bed479a76cb759acb
Marko Tanasijevic <tanasije286@gmail.ca>
2024-10-18 09:17:17 -0400

    Moved board logic upward to game component

--- src/App.js
+++ src/App.js
@@ -9,18 +9,14 @@ function Square({ value , onSquareClick}) {
     );
 }
 
-function Board() {
-    const [xIsNext, setXIsNext] = useState(true);
-    const [squares, setSquares] = useState(Array(9).fill(null));
-
+function Board({ xIsNext, squares, onPlay }) {
     function handleClick(i){
         if (squares[i] || calculateWinner(squares)) {
             return;
         }
         const nextSquares = squares.slice();
         (xIsNext ? nextSquares[i] = 'X' : nextSquares[i] = 'O');
-        setSquares(nextSquares);
-        setXIsNext(!xIsNext);
+        onPlay(nextSquares);
     }
 
     const winner = calculateWinner(squares);
@@ -54,10 +50,19 @@ function Board() {
 }
 
 function Game() {
+    const [xIsNext, setXIsNext] = useState(true);
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const currentSquares = history[history.length - 1];
+
+    function handlePlay(nextSquares) {
+        setHistory([...history, nextSquares]);
+        setXIsNext(!xIsNext);
+    }
+
     return (
         <div className="game">
             <div className="game-board">
-                <Board />
+                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
             </div>
             <div className="game-info">
                 <ol>{/*TODO*/}</ol>
