1d2b78fcf4e4a5babbab64ae03cbe2d768495273
Marko Tanasijevic <tanasije286@gmail.ca>
2024-10-18 14:35:48 -0400

    Board updated to be listed using 2 loops instead of hardcoded

--- src/App.js
+++ src/App.js
@@ -27,24 +27,19 @@ function Board({ xIsNext, squares, onPlay }) {
         status = "Next player: " + (xIsNext ? "X" : "O");
     }
 
+    const listBoardRow = [0,1,2]
+    const listBoardCol = [0,1,2]
+
     return (
         <>
             <div className="status">{status}</div>
-            <div className="board-row">
-                <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-                <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-                <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-            </div>
-            <div className="board-row">
-                <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-                <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-                <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-            </div>
-            <div className="board-row">
-                <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-                <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-                <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-            </div>
+            {listBoardRow.map(row => (
+                <div className="board-row">
+                    {listBoardCol.map(col =>(
+                        <Square value={squares[row*3+col]} onSquareClick={() => handleClick(row*3+col)}/>
+                    ))}
+                </div>
+            ))}
         </>
     );
 }
